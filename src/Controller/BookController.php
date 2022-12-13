e1463fffb4e20aeb1d5d8800473680af8060ab93
Oumarou Ado Mahamadou <moumarou93@gmail.com>
2022-12-13 16:21:42 +0100

    DELETE AND ADD API

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Repository\BookRepository;
 use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,32 +23,52 @@ class BookController extends AbstractController
         $bookList = $bookRepository->findAll();
         $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
 
+        // Retourne 200
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 
     /**
-     * @Route("/api/books/{id}", name="book", methods="GET")
+     * @Route("/api/books/{id}", name="detailBook", methods="GET")
      */
-//    public function getOneBook(int $id, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
-//    {
-//        $book       = $bookRepository->find($id);
-//        // Si le livre est trouvé
-//        if($book) {
-//            // Format JSON
-//            $jsonBook   = $serializer->serialize($book, 'json');
-//            // Retour OK
-//            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
-//        }
-//        // Livre non trouvé
-//        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
-//    }
-
     public function getOneBook(Book $book, SerializerInterface $serializer): JsonResponse
     {
         // Format JSON
         $jsonBook   = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
-        // Retour OK
+        // Retourne 200
         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/api/books/{id}", name="deleteBook", methods="DELETE")
+     */
+    public function deleteBook(Book $book, EntityManagerInterface $em): JsonResponse
+    {
+        // Suppression
+        $em->remove($book);
+        // Confirmation
+        $em->flush();
+        // Retourne 204
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+
+    /**
+     * @Route("/api/books", name="addBook", methods="POST")
+     */
+    public function addBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        // Deserialise en JSON
+        $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        // On enregistre
+        $em->persist($book);
+        // On confirme l'enregistrement
+        $em->flush();
+        // Format JSON
+        $jsonBook   = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
+        // Redirection : Location au niveau de header
+        $location = $urlGenerator->generate('detailBook', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        // Retour OK
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ['Location' => $location], true);
     }
 
 
