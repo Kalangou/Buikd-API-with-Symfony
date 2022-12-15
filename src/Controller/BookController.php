4b92f492629a03fe8d727e20d72c2fcbf0df0ef3
Oumarou Ado Mahamadou <moumarou93@gmail.com>
2022-12-15 10:44:17 +0100

    End CRUD BOOK

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
 use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -55,10 +57,18 @@ class BookController extends AbstractController
     /**
      * @Route("/api/books", name="addBook", methods="POST")
      */
-    public function addBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    public function addBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em,
+        UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository): JsonResponse
     {
         // Deserialise en JSON
         $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        // Recuperation de l'ensemble des données sous forme tableau
+        $content = $request->toArray();
+        // Recuperation de l'id author, s'il n'est pas defini alors on met -1 par defaut
+        $idAuthor = $content['idAuthor'] ?? -1;
+        // On cherche l'auteur qui correspont et on l'associe à ce livre
+        // Si find ne trouve pas ID alors null sera renvoyé
+        $book->setAuthor($authorRepository->find($idAuthor));
         // On enregistre
         $em->persist($book);
         // On confirme l'enregistrement
@@ -72,4 +82,26 @@ class BookController extends AbstractController
     }
 
 
+    /**
+     * @Route("/api/books/{id}", name="updateBook", methods="PUT")
+     */
+    public function updateBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em,
+                            Book $currentBook, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository): JsonResponse
+    {
+        // Deserialise en JSON
+        $updateBook = $serializer->deserialize($request->getContent(), Book::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentBook]);
+        // Recuperation de l'ensemble des données sous forme tableau
+        $content = $request->toArray();
+        // Recuperation de l'id author, s'il n'est pas defini alors on met -1 par defaut
+        $idAuthor = $content['idAuthor'] ?? -1;
+        // On cherche l'auteur qui correspont et on l'associe à ce livre
+        // Si find ne trouve pas ID alors null sera renvoyé
+        $updateBook->setAuthor($authorRepository->find($idAuthor));
+        // On enregistre
+        $em->persist($updateBook);
+        // On confirme l'enregistrement
+        $em->flush();
+        // Retour OK
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
